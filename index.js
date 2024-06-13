0195a20c8e030622c5d64521150733cd207ce2d9
Sakhawat Hossen Sakib <sakibsakhawat21@gmail.com>
2024-06-13 22:42:13 +0600

    payment applied

--- index.js
+++ index.js
@@ -198,6 +198,7 @@ async function run() {
     // payment intent
     app.post("/create-payment-intent", async (req, res) => {
       const { price } = req.body;
+
       const amount = parseInt(price * 100);
       console.log(amount, "amount inside the intent");
 
